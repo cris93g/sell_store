70074cd1668f48a6fcb5d4c47aaf92a407258ffb
cris93g <37388115+cris93g@users.noreply.github.com>
2019-07-11 15:05:25 -0500

    added tvs to front end

--- src/pages/Tvs/Tvs.js
+++ src/pages/Tvs/Tvs.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getTv } from "../../redux/ducks/itemReducer";
 
 class Tvs extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return <div>tvs</div>;
-	}
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.props.getTv();
+  }
+  render() {
+    console.log(this.props.itemReducer);
+    return <div>tvs</div>;
+  }
 }
-export default Tvs;
+
+const mapStateToProps = state => state;
+export default connect(
+  mapStateToProps,
+  { getTv }
+)(Tvs);
