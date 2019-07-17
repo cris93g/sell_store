389e7c20974507c07f4f3f20cac2f7cd372f66eb
cris93g <37388115+cris93g@users.noreply.github.com>
2019-07-17 12:48:26 -0500

    added style

--- src/components/Checkout/Payment.js
+++ src/components/Checkout/Payment.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { injectStripe, CardElement } from "react-stripe-elements";
 import axios from "axios";
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 
 class Payment extends Component {
@@ -15,73 +15,73 @@ class Payment extends Component {
       error: false,
       message: ""
     };
-    // this.style = {
-    //   form: {
-    //     width: "800px",
-    //     margin: "0 auto",
-    //     backgroundColor: "#f7f8f9", //"#32325d",
-    //     padding: "10px"
-    //   },
-    //   childForm: {
-    //     display: "flex"
-    //   },
-    //   inputContainer: {
-    //     flexGrow: 1,
-    //     display: "flex"
-    //   },
-    //   error: {
-    //     color: "red",
-    //     background: "#ffff0099",
-    //     borderRadius: "5px",
-    //     padding: "20px",
-    //     margin: "5px 0px",
-    //     fontSize: "20px",
-    //     border: "1px red soild"
-    //   },
-    //   button: {
-    //     "::hover": {
-    //       boxShadow:
-    //         " 0 7px 14px rgba(50, 50, 93, .10), 0 3px 6px rgba(0, 0, 0, .08)",
-    //       backgroundColor: "#43458b"
-    //     },
-    //     border: "none",
-    //     outline: "none",
-    //     color: "#fff",
-    //     borderRadius: "5px",
-    //     transition: "all 150ms ease",
-    //     background: "#32325d",
-    //     padding: "0 14px",
-    //     fontSize: "20px",
-    //     height: "45px",
-    //     width: "165px"
-    //   },
-    //   input: {
-    //     height: "45px",
-    //     borderRadius: "5px",
-    //     border: "none",
-    //     padding: "12px 10px 6px",
-    //     margin: "5px",
-    //     fontSize: "20px",
-    //     width: "100%",
-    //     flexGrow: 1
-    //   },
-    //   stripe: {
-    //     base: {
-    //       color: "#32325d",
-    //       lineHeight: "18px",
+    this.style = {
+      form: {
+        width: "800px",
+        margin: "0 auto",
+        backgroundColor: "#f7f8f9", //"#32325d",
+        padding: "10px"
+      },
+      childForm: {
+        display: "flex"
+      },
+      inputContainer: {
+        flexGrow: 1,
+        display: "flex"
+      },
+      error: {
+        color: "red",
+        background: "#ffff0099",
+        borderRadius: "5px",
+        padding: "20px",
+        margin: "5px 0px",
+        fontSize: "20px",
+        border: "1px red soild"
+      },
+      button: {
+        "::hover": {
+          boxShadow:
+            " 0 7px 14px rgba(50, 50, 93, .10), 0 3px 6px rgba(0, 0, 0, .08)",
+          backgroundColor: "#43458b"
+        },
+        border: "none",
+        outline: "none",
+        color: "#fff",
+        borderRadius: "5px",
+        transition: "all 150ms ease",
+        background: "#32325d",
+        padding: "0 14px",
+        fontSize: "20px",
+        height: "45px",
+        width: "165px"
+      },
+      input: {
+        height: "45px",
+        borderRadius: "5px",
+        border: "none",
+        padding: "12px 10px 6px",
+        margin: "5px",
+        fontSize: "20px",
+        width: "100%",
+        flexGrow: 1
+      },
+      stripe: {
+        base: {
+          color: "#32325d",
+          lineHeight: "18px",
 
-    //       fontSmoothing: "antialiased",
-    //       fontSize: "22px",
-    //       "::placeholder": {
-    //         color: "#aab7c4"
-    //       }
-    //     },
-    //     invalid: {
-    //       color: "#fa755a",
-    //       iconColor: "#fa755a"
-    //     }
-    //   }
-    // };
+          fontSmoothing: "antialiased",
+          fontSize: "22px",
+          "::placeholder": {
+            color: "#aab7c4"
+          }
+        },
+        invalid: {
+          color: "#fa755a",
+          iconColor: "#fa755a"
+        }
+      }
+    };
   }
 
   getPaid(e) {
@@ -113,9 +113,13 @@ class Payment extends Component {
 
   render() {
     return (
-      <form name="payment" onSubmit={e => this.getPaid(e)}>
+      <form
+        name="payment"
+        style={this.style.form}
+        onSubmit={e => this.getPaid(e)}
+      >
         {this.state.error ? (
-          <div>
+          <div style={this.style.error}>
             <p>
               Failed to complete Transation:
               {this.state.message}
@@ -123,49 +127,52 @@ class Payment extends Component {
           </div>
         ) : null}
         <label> Payment Information </label>
-        <div>
-          <div>
+        <div style={this.style.childForm}>
+          <div style={this.style.inputContainer}>
             <input
               name="firstName"
               placeholder="First Name"
+              style={this.style.input}
               onChange={e => this.handleChange({ firstName: e.target.value })}
             />
           </div>
-          <div>
+          <div style={this.style.inputContainer}>
             <input
               name="lastName"
               placeholder="Last Name"
+              style={this.style.input}
               onChange={e => this.handleChange({ lastName: e.target.value })}
             />
           </div>
         </div>
-        <div>
-          <div>
+        <div style={this.style.childForm}>
+          <div style={this.style.inputContainer}>
             <input
               name="address"
               placeholder="Address"
+              style={this.style.input}
               onChange={e => this.handleChange({ address: e.target.value })}
             />
           </div>
         </div>
-
         <label>
           Card Details
-          <br />
-          <CardElement />
+          <CardElement style={this.style.stripe} />
         </label>
         <div>
-          <Link to={"/Confirmation"}>
-            <button type="submit">Submit</button>
-          </Link>
+        <Link to={"/Confirmation"}>
+          <button type="submit" style={this.style.button}>
+            Submit
+          </button>
+            </Link>
         </div>
       </form>
     );
   }
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     total: state.total
-  };
-};
+  }
+}
 export default connect(mapStateToProps)(injectStripe(Payment));
